8b4d6c8fe8686ed97688289a19ab9be7fdd603a8
Stancu Denisa <101021088+StancuDenisaG@users.noreply.github.com>
2022-10-28 14:33:03 +0300

    Add files via upload

---
+++ Homework 1/homework1.ino
@@ -0,0 +1,38 @@
+const int redPotPin = A0;
+const int greenPotPin = A1;
+const int bluePotPin = A2;
+
+const int redLedPin = 9;
+const int greenLedPin = 10;
+const int blueLedPin = 11;
+
+const int maxAnalogValue = 1023;
+const int maxLedValue = 255;
+
+int redPotValue, greenPotValue, bluePotValue, redLedValue, greenLedValue, blueLedValue;
+
+void setup() {
+  pinMode(redPotPin, INPUT);
+  pinMode(greenPotPin, INPUT);
+  pinMode(bluePotPin, INPUT);
+
+  pinMode(redLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(blueLedPin, OUTPUT);
+
+}
+
+void loop() {
+  redPotValue = analogRead(redPotPin);
+  greenPotValue = analogRead(greenPotPin);
+  bluePotValue = analogRead(bluePotPin);
+
+  redLedValue = map(redPotValue, 0, maxAnalogValue, 0, maxLedValue);
+  greenLedValue = map(greenPotValue, 0, maxAnalogValue, 0, maxLedValue);
+  blueLedValue = map(bluePotValue, 0, maxAnalogValue, 0, maxLedValue);
+
+  analogWrite(redLedPin, redLedValue);
+  analogWrite(greenLedPin, greenLedValue);
+  analogWrite(blueLedPin, blueLedValue);
+
+}
